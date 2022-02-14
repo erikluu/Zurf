4a336cd0347b73c7b3fc32f15faceb9cd294fd92
crugggiero <ruggicar555@gmail.com>
2022-02-14 10:45:39 -0800

    update map 1

--- map-view/src/App.js
+++ map-view/src/App.js
@@ -8,10 +8,11 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY3J1Z2dnaWVybyIsImEiOiJja3pteWFjMTAxZ2k3Mndue
 
 
 function App() {
+
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
+  const [lng, setLng] = useState(-120.6252);
+  const [lat, setLat] = useState(35.2628);
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
@@ -24,11 +25,23 @@ function App() {
     });
   });
 
-return (
-  <div>
-    <div ref={mapContainer} className="map-container" />
-  </div>
-);
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+    setLng(map.current.getCenter().lng.toFixed(4));
+    setLat(map.current.getCenter().lat.toFixed(4));
+    setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div>
+      <div className='sidebar'>
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 
 }
 
