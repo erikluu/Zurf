6b4122bb73c01a4bd94796e929bbf57f4723a279
Erik Luu <63026434+erikluu@users.noreply.github.com>
2022-02-14 11:37:34 -0800

    card1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import React, { useRef, useEffect, useState } from "react";
 import Map from "./components/Map.js";
 import SurfSpotCard from "./components/SurfSpotCard";
 
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>;
+
 function App() {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import "semantic-ui-css/semantic.min.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.render(
   <React.StrictMode>
