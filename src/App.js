4933b3a4ce4f38362c32baaca7ef7bde2a28fbc3
crugggiero <ruggicar555@gmail.com>
2022-02-14 11:39:45 -0800

    loc user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useRef, useEffect, useState } from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import Map from "./components/Map.js";
 
+
 function App() {
   return <Map />;
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -30,6 +30,21 @@ function Map() {
     });
   });
 
+  useEffect(() => {
+    if (map.current.GeolocateControl) return;
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+          positionOptions: {
+              enableHighAccuracy: true
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: true
+      })
+    );
+  });
+
   return (
     <div>
       <div className="sidebar">
