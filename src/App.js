42ee0219b58333f028195666bebcf99c68744225
Erik Luu <63026434+erikluu@users.noreply.github.com>
2022-02-14 11:14:53 -0800

    yea

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import "./App.css";
 import React, { useRef, useEffect, useState } from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import Map from "./components/Map.js";
+import SurfSpotCard from "./components/SurfSpotCard";
 
 function App() {
-  return <Map />;
+  return (
+    <div>
+      <Map />
+      <SurfSpotCard />
+    </div>
+  );
 }
 
 export default App;
