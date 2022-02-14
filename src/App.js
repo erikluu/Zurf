8a4402eb7416ec8702e88b28c3d1b021b19161e8
Erik Luu <63026434+erikluu@users.noreply.github.com>
2022-02-14 10:57:48 -0800

    clean up

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import React, { useRef, useEffect, useState } from "react";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import Map from "./components/Map.js";
+
+function App() {
+  return <Map />;
+}
+
+export default App;
---
+++ src/components/SurfLocationFrame.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function SurfLocationFrame() {
+  return <h1>Hello THERE</h1>;
+}
+
+export default SurfLocationFrame;
