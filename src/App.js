dac6f4b1fef80ed5b24410061e16bf725c652ca3
Erik Luu <63026434+erikluu@users.noreply.github.com>
2022-02-27 14:37:25 -0800

    temp login/router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import "./App.css";
 import React, { useRef, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard.js";
+import LoginForm from "./components/LoginForm";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import Map from "./components/Map.js";
-import SurfSpotCard from "./components/SurfSpotCard";
 
 <link
   rel="stylesheet"
   href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 />;
 
+const testUser = {
+  email: "carlo@dumb.com",
+  password: "imstupid",
+};
 
 function App() {
-  return (
-    <div>
-      <Map />
-      <SurfSpotCard />
-    </div>
-  );
+  const [user, setUser] = useState({ email: "" });
+  const [error, setError] = useState("");
+  const Login = (details) => {
+    console.log(details);
+    if (details.email === testUser.email && details.password === testUser.password) {
+      console.log("logged in");
+      setUser({ email: details.email });
+    } else {
+      console.log("details do not match");
+      setError("Details do not match.");
+    }
+  };
+
+  const Logout = () => {
+    console.log("Logged out.");
+  };
+
+  return <div>{user.email !== "" ? <Dashboard /> : <LoginForm Login={Login} error={error} />}</div>;
 }
 
 export default App;
