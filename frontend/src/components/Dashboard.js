33b64967ffac2f9033a266f100045221fe24e34a
JackOrt <44760123+JackOrt@users.noreply.github.com>
2022-03-11 00:17:33 -0800

    Update Dashboard.js
    Update spot icon to fetch and display (through an alert for now) the most recent daily forecast from api.weather.gov for that point. (Easily can be changed return forecasts from up to 165 different discrete time periods, for now the forecast only includes the 0th forecast.)

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -13,9 +13,7 @@ const morroRockLNG = -120.864096;
 const morroRockLAT = 35.373504;
 const pismoLNG = -120.643497;
 const pismoLAT = 35.138778;
-const avilaLNG = -120.7318418; 
-const avilaLAT = 35.1799752;
-
+var pointForecastURL = "";
 
 // possible schema for storing beach locations
 // displays on the map based on lat/long
@@ -47,29 +45,16 @@ const beachList = {
       },
       img: "https://keyt.b-cdn.net/2020/09/118794055_1429416193923564_3229598932206464322_n-1.jpg",
     },
-    {
-      type: "Beach",
-      properties: {
-        message: "Avila",
-        iconSize: [60, 60],
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [avilaLNG, avilaLAT],
-      },
-      img: "https://image.arrivalguides.com/415x300/11/ca019aca3f54a23395d704303a7dfb5c.jpg"
-    },
   ],
 };
 
 function Dashboard() {
-
-  // const [userDetails, setUserDetails] = useState(() => {
-  //   // getting stored value
-  //   const saved = localStorage.getItem("user_details");
-  //   const initialValue = JSON.parse(saved);
-  //   return initialValue || "";
-  // });
+  const [userDetails, setUserDetails] = useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem("user_details");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
 
 //   let user = userDetails.data.users_list[0];
 //   console.log(user);
@@ -122,7 +107,7 @@ function Dashboard() {
       mark.style.backgroundSize = "100%";
 
       mark.addEventListener("click", () => {
-        window.alert(marker.properties.message);
+         fetchWeatherData(marker.geometry.coordinates[1], marker.geometry.coordinates[0]);
       });
       // Add markers to the map.
       new mapboxgl.Marker(mark).setLngLat(marker.geometry.coordinates).addTo(map.current);
@@ -198,6 +183,60 @@ function Dashboard() {
     });
  
   });
+
+  function fetchPointDailyForecast(URL) {
+   var pointForecast;
+
+   new Promise(function (resolve, reject) { 
+   fetch(URL)
+   .then(function (response) {
+     if (response.status === 200) {
+       return response.json();
+     } else {
+        console.log(URL);
+        console.log("error fetching forecast data.");
+       reject(response.status);
+     }
+   })
+   .then(function (parsedResponse) {
+     pointForecast = JSON.stringify(parsedResponse.properties.periods[0]);
+     resolve(parsedResponse);
+
+     console.log(pointForecast);
+     window.alert(pointForecast);
+   });
+});
+  }
+
+  function fetchWeatherData(latitude, longitude) {
+   new Promise(function (resolve, reject) {
+     fetch(
+       "https://api.weather.gov/points/" +
+         latitude +
+         "," +
+         longitude)
+       .then(function (response) {
+         if (response.status === 200) {
+           return response.json();
+         } else {
+            console.log(
+               "https://api.weather.gov/points/" +
+                 latitude +
+                 "," +
+                 longitude)
+            console.log("error fetching weather data.")
+           reject(response.status);
+         }
+       })
+       .then(function (parsedResponse) {
+         pointForecastURL = parsedResponse.properties.forecast;
+         console.log(pointForecastURL);
+         resolve(parsedResponse);
+         fetchPointDailyForecast(pointForecastURL);
+      })
+   });
+
+}
 
   return (
     <div>
