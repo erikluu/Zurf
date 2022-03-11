cd6d2583582a06a7add87950686d18697bee0355
crugggiero <ruggicar555@gmail.com>
2022-03-10 21:20:09 -0800

    uh

--- frontend/src/components/SidebarData.js
+++ frontend/src/components/SidebarData.js
@@ -6,7 +6,7 @@ import * as GiIcons from 'react-icons/gi';
 export const SidebarData = [
   {
     title: 'Home',
-    path: '/',
+    path: '/dashboard',
     icon: <AiIcons.AiFillHome />,
     cName: 'nav-text'
   },
