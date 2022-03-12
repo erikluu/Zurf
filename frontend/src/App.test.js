3d0a5a0a06c9dccba8ab69dbc7cb2e24d0a0d903
crugggiero <ruggicar555@gmail.com>
2022-03-11 23:02:08 -0800

    update tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,54 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from "react";
 
-test('renders Zurf Title', () => {
+test('App renders Zurf Title', () => {
   render(<App />);
   const linkElement = screen.getByText(/Zurf/);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('App renders nav bar home', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Home/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('App renders nav bar account', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Account/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('App renders nav bar support', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Support/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('App renders nav bar loc1', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Location 1/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('App renders nav bar loc 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Location 2/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+//need to test react hooks in App
+
+// const mockSetState = jest.fn();
+
+// jest.mock('react', () => ({
+//   useState: initial => [initial, mockSetState]
+// }));
+
+// test('can update user', () => {
+//   const [_, setUser] = App.getUser();
+//   App.getUser("dave");
+//   expect(mockSetState).toHaveBeenCalledWith("dave");
+// });
